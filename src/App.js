49665e9d3e39c4a2201cb11e21badacdf77e54fc
Jill Marcum <jill.marcum@gmail.com>
2020-03-05 22:32:48 +0000

    updated app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+/*
 function App() {
   return (
     <div className="App">
@@ -22,5 +48,5 @@ function App() {
     </div>
   );
 }
+*/
 
-export default App;
